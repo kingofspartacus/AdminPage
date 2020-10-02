03fcb63d3444041bf6e326643d0d1bd1208f318b
ductt <ductt@fabbi.io>
2020-10-02 16:51:06 +0700

    new

--- src/pages/dashboards/Default/index.js
+++ src/pages/dashboards/Default/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "reactstrap";
 
 import Statistics from "./Statistics";
@@ -10,37 +10,40 @@ import Appointments from "./Appointments";
 import Projects from "./Projects";
 import BarChart from "./BarChart";
 
-const Default = () => (
-  <Container fluid className="p-0">
-    <Statistics />
-    <Row>
-      <Col lg="8" className="d-flex">
-        <LineChart />
-      </Col>
-      <Col lg="4" className="d-flex">
-        <Feed />
-      </Col>
-    </Row>
-    <Row>
-      <Col lg="6" xl="4" className="d-flex">
-        <Calendar />
-      </Col>
-      <Col lg="6" xl="4" className="d-flex">
-        <PieChart />
-      </Col>
-      <Col lg="6" xl="4" className="d-flex">
-        <Appointments />
-      </Col>
-    </Row>
-    <Row>
-      <Col lg="6" xl="8" className="d-flex">
-        <Projects />
-      </Col>
-      <Col lg="6" xl="4" className="d-flex">
-        <BarChart />
-      </Col>
-    </Row>
-  </Container>
-);
+// const Default = () => (
+//   <Container fluid className="p-0">
+
+//     <Row>
+//       <Col lg="6" xl="12" className="d-flex">
+//         <Projects />
+//       </Col>
+
+//     </Row>
+//   </Container>
+// );
+
+
+const Default = () => {
+  const isLogin = localStorage.getItem("token")
+  const [page, setPage] = useState(10)
+  const [perPage, setPerPage] = useState(1)
+
+  useEffect(() => {
+    console.log(isLogin, "sda")
+    if (!isLogin) {
+      window.location.replace('/auth/sign-in')
+    }
+    return
+  }, [])
+  return (
+    <Container fluid className="p-0">
+      <Row>
+        <Col lg="6" xl="12" className="d-flex">
+          <Projects />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
 
 export default Default;
