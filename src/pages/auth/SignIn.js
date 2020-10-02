f86d0115d5c3d506f95ee0054f6fdaddc68062ab
ductt <ductt@fabbi.io>
2020-10-02 16:37:46 +0700

    first commit

---
+++ src/pages/auth/SignIn.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios'
+import { BASE_URL } from '../../utils/config'
+import {
+  Button,
+  Card,
+  CardBody,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  CustomInput
+} from "reactstrap";
+import { toastr } from 'react-redux-toastr'
+
+import avatar from "../../assets/img/avatars/avatar.jpg";
+
+const SignIn = () => {
+  const [userName, setUserName] = useState("admindev")
+  const [pass, setPassWord] = useState("Abc@123")
+  const handleLogin = () => {
+    // const dataConfig ={
+    //   type: "ADMIN",
+    //   "password": "Abc@123",
+    //   "username": "admindev"
+    // }
+    const dataConfig = {
+      type: "ADMIN",
+      password: pass,
+      username: userName
+    }
+    axios.post(BASE_URL, dataConfig)
+      .then(res => {
+        res.data && toastr.success("Thông báo", "Đăng nhập thành công")
+        localStorage.setItem("token",res.data?.data?.accessToken)
+        window.location.replace('/')
+      })
+      .catch(err => toastr.error("Cảnh báo", "Đăng nhập không hợp lệ vui lòng thử lại"))
+  }
+  return (
+    < React.Fragment >
+      <Card>
+        <CardBody>
+          <div className="m-sm-4">
+            <Form>
+              <FormGroup>
+                <Label>Tên đăng nhập</Label>
+                <Input
+                  bsSize="lg"
+                  placeholder="Nhập tên đăng nhập "
+                  defaultValue={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label>Mật khẩu</Label>
+                <Input
+                  bsSize="lg"
+                  type="password"
+                  name="password"
+                  defaultValue={pass}
+                  placeholder="Nhập mật khẩu"
+                  onChange={(e) => setPassWord(e.target.value)}
+
+                />
+
+              </FormGroup>
+
+              <div className="text-center mt-3">
+                <Button color="primary" size="lg" onClick={handleLogin}>
+                  Sign in
+                </Button>
+              </div>
+            </Form>
+          </div>
+        </CardBody>
+      </Card>
+    </React.Fragment >
+  )
+
+}
+
+
+
+
+
+export default SignIn;
---
+++ src/utils/config.js
@@ -0,0 +1,1 @@
+export const BASE_URL = "http://178.128.61.95:8888"
